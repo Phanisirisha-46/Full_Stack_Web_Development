36bf6272981c8210be842c5bd87e745eaf5e7c1c
Phanisirisha-46 <tadikamallaharshith2003@gmail.com>
2024-06-04 17:10:13 +0530

    assignment25_2

--- assignment25_2/assignment25_2.js
+++ assignment25_2/assignment25_2.js
@@ -1,19 +1,45 @@
-const form=document.querySelector("form");
-const h1=document.querySelector('.result');
-let firstInt=form.children[0];
-let secondInt=form.children[1];
-let btn1=form.children[2];
-let btn2=form.children[3];
-let btn3=form.children[4];
-let btn4=form.children[5];
-let btn5=form.children[6];
+const form = document.getElementById('calculator-form');
+const h1 = document.querySelector('.result');
+const firstInt = form.querySelector('input[name="first"]');
+const secondInt = form.querySelector('input[name="second"]');
+const btnAdd = document.getElementById('add');
+const btnSubtract = document.getElementById('subtract');
+const btnMultiply = document.getElementById('multiply');
+const btnModulus = document.getElementById('modulus');
+const btnDivide = document.getElementById('divide');
 
+// Add click event listeners to each button
+btnAdd.addEventListener('click', () => handleButtonClick('+'));
+btnSubtract.addEventListener('click', () => handleButtonClick('-'));
+btnMultiply.addEventListener('click', () => handleButtonClick('*'));
+btnModulus.addEventListener('click', () => handleButtonClick('%'));
+btnDivide.addEventListener('click', () => handleButtonClick('/'));
 
-form.addEventListener("submit",(event)=>{
-    event.preventDefault();
+// Function to handle button clicks
+function handleButtonClick(operation) {
+    let v1 = Number(firstInt.value);
+    let v2 = Number(secondInt.value);
+    let answer;
 
-let v1=Number(firstInt.value);
-let v2=Number(secondInt.value);
+    switch (operation) {
+        case '+':
+            answer = v1 + v2;
+            break;
+        case '-':
+            answer = v1 - v2;
+            break;
+        case '*':
+            answer = v1 * v2;
+            break;
+        case '%':
+            answer = v1 % v2;
+            break;
+        case '/':
+            answer = v1 / v2;
+            break;
+        default:
+            answer = 'Invalid operation';
+    }
 
-
-});
+    h1.textContent = answer;
+}
