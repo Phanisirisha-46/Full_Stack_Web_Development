e1ecf0d0b18c75ce30b9fe1693df1d126c46007c
Phanisirisha-46 <veerankiphanisirisha@gmail.com>
2024-07-06 16:59:39 +0530

    backend_mongodb

--- backend1/server.js
+++ backend1/server.js
@@ -2,6 +2,21 @@
  //import express module
  const exp=require('express');
  const app=exp();
+//import mongoclient
+const {MongoClient}=require('mongodb');
+//connect to mongodb server
+
+let mclient=new MongoClient('mongodb://127.0.0.1:27017')
+  mclient.connect().then(()=>{
+   console.log("db connection success")
+   app.listen(4000,()=>{
+      console.log('http sever started on port 4000');
+   })
+})
+  .catch(err=>console.log("err in db connection",err))
+
+
+
 
  const userapp=require('./API/userApi');
  const proapp=require('./API/productApi');
@@ -10,6 +25,3 @@
  app.use('/product-api',proapp);
 
  //assign port number to http server of express app
- app.listen(4000,()=>{
-    console.log('http sever started on port 4000');
- })
